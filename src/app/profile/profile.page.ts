7097c6231fcbcd4e5b34e252fb965ce9541c2c05
PabloR0driguez084 <juanp-rodriguezm@unilibre.edu.co>
2025-04-21 20:04:03 -0500

    Creacion de perfil cuando se registra
    Se creo la funcion de crear un perfil que el usuario puede configurar cuando se haya resgistrado.

---
+++ src/app/profile/profile.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { doc, docData, updateDoc } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+  userProfile: any = {
+    name: '',
+    age: null,
+    photoUrl: '',
+    description: ''
+  };
+  uid: string | null = null;
+
+  constructor(private auth: Auth, private firestore: Firestore) {}
+
+  async ngOnInit() {
+    const user = this.auth.currentUser;
+    if (user) {
+      this.uid = user.uid;
+      const profileRef = doc(this.firestore, 'users', this.uid);
+      docData(profileRef).subscribe(data => {
+        this.userProfile = data;
+      });
+    }
+  }
+
+  async updateProfile() {
+    if (this.uid) {
+      const profileRef = doc(this.firestore, 'users', this.uid);
+      await updateDoc(profileRef, {
+        name: this.userProfile.name,
+        age: this.userProfile.age,
+        photoUrl: this.userProfile.photoUrl,
+        description: this.userProfile.description,
+      });
+    }
+  }
+}
